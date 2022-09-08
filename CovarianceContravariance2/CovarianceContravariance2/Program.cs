bbf6ac614e3964744876d7d3260446c55f1ba781
ShubhankarIntsof <shubhankar.shubham@intsof.com>
2022-09-08 12:42:41 +0530

    Compare Done

--- CovarianceContravariance2/CovarianceContravariance2/Program.cs
+++ CovarianceContravariance2/CovarianceContravariance2/Program.cs
@@ -2,11 +2,21 @@
 
 namespace CovarianceContravariance2
 {
-    class Program
+
+    internal class Program
     {
+        static void Setobject(object val1) {
+            Console.WriteLine($"Inside SetObject + {val1}");
+        }
+        static void SetString(string val2) { }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Contravariance delegates reverses the covariance functionality");
+            Action<string> del1 = Setobject;
+            del1("Hello");
+           //Console.WriteLine();
+
         }
     }
 }
