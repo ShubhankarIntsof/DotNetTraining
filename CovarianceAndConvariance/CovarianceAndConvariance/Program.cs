faaf034e0106a3dcd1ab7c62e87dba210890d88c
ShubhankarIntsof <shubhankar.shubham@intsof.com>
2022-09-08 09:35:33 +0530

    Eight Sept One

--- CovarianceAndConvariance/CovarianceAndConvariance/Program.cs
+++ CovarianceAndConvariance/CovarianceAndConvariance/Program.cs
@@ -4,9 +4,33 @@ namespace CovarianceAndConvariance
 {
     class Program
     {
+        static void SetObject(Object val)
+        {
+
+        }
+        static void SetObject(String val)
+        {
+
+        }
+        static string GetString() { return "Inside GetString()"; }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Covariance and Contravariance");
+            Console.WriteLine("Array data type supports Convariance");
+
+            object[] objArray = new string[10];
+
+            objArray[0] = "10";
+            Console.WriteLine(objArray[0]);
+
+            Console.WriteLine("Delegates covariance");
+
+            Func<object> delegateobj = GetString;
+            //String strobj = delegateobj;
+
+            Console.WriteLine("Contravariance Delegate reverses the Covariance functionaloty");
+            Action<string> del2 = SetObject;
+
+        }  
     }
 }
