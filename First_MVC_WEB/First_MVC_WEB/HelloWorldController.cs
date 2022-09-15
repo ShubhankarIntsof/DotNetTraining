3f9e83079bf5fd2cf3c9cf8f150b31319522713a
ShubhankarIntsof <shubhankar.shubham@intsof.com>
2022-09-15 09:09:43 +0530

    MVC

---
+++ First_MVC_WEB/First_MVC_WEB/HelloWorldController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First_MVC_WEB
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorldController
+        public string Index()
+        {
+            //return View();
+            return "I am Shubhankar";
+        }
+
+        // GET: HelloWorldController/Details/5
+        public string Details(string id)
+        {
+            return "I am Shubhankar " + id;              
+        }
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //    //return "I am Shubhankar";
+        //}
+
+        //// GET: HelloWorldController/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        // GET: HelloWorldController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HelloWorldController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HelloWorldController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HelloWorldController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HelloWorldController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HelloWorldController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ LINQ_Framework/LINQ_Framework/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LINQ_Framework
+{
+    class Program
+    {   //Step1: Connection String
+        string path = "C:\\Users\\Shubhankar Shubham\\source\\repos\\LINQ_Framework\\LINQ_Framework\\ParticipantDetails.xml";
+        private void GetXml()
+        {
+            try
+            {
+                //Step2:Loading
+                XDocument xDocument = XDocument.Load(path);
+                //XDocument xDocument = XDocument.Parse(path);
+                //Console.WriteLine("ROOT" + xDocument.Root);
+                var Students = from participant in xDocument.Descendants("Participant")
+                               select new
+                               {
+                                   ID = Convert.ToInt32(participant.Attribute("ID").Value),
+                                   Name = participant.Element("Name").Value
+                               };
+                //Console.WriteLine(Students.ToString());
+                foreach (var student in Students)
+                {
+                    Console.WriteLine("--");
+                    Console.WriteLine(student.ID + " - " + student.Name);
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                //throw;
+            }
+
+
+
+        }     
+
+        private void SetXml(string name)
+        {
+            try
+            {   //loading the document
+                XDocument myXml = XDocument.Load(path);
+                XElement newParticipant = new XElement("Participant",new XElement("Name",name));
+                var LastStudent = myXml.Descendants("Participant").Last();
+                int newID = Convert.ToInt32(LastStudent.Attribute("ID").Value);
+
+                newParticipant.SetAttributeValue("ID", newID + 1);
+                myXml.Element("Participants").Add(newParticipant);
+                myXml.Save(path);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                //throw;
+            }
+        }
+
+        private void ModifyXmlData(string name, int id)
+        {
+            try
+            {   //loading the document
+                XDocument myXml = XDocument.Load(path);
+                var ListStudent = myXml.Descendants("Participant").Where(c => c.Attribute("ID").Value.Equals(id.ToString())).FirstOrDefault();
+
+                ListStudent.Element("Name").Value = name;
+                myXml.Save(path);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                //throw;
+            }
+
+        }
+
+        private void DeleteXml(int id)
+        {
+            try
+            {   //loading the document
+                XDocument myXml = XDocument.Load(path);
+                var DStudent = myXml.Descendants("Participant").Where(c => c.Attribute("ID").Value.Equals(id.ToString())).FirstOrDefault();
+
+                DStudent.Remove();
+                myXml.Save(path);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                //throw;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ TO XML");
+
+            Program obj1 = new Program();
+            //obj1.GetXml();
+
+            //obj1.SetXml("Sohan");
+            //obj1.GetXml();
+
+            //obj1.ModifyXmlData("gmail", 1);
+            obj1.DeleteXml(1);
+            obj1.GetXml();
+
+        }
+    }
+}
